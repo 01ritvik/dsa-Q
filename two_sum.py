97b2e4f3162c272c7901c37ece269a88ee352545
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-10-18 09:57:53 +0530

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+class solution:
+    def twosums(self, nums, target):
+
+        store = {}
+
+        for i in range (len(nums)):
+            if nums[i] in store:
+                return [store[nums[i]] , i]
+            else:
+                store[target-nums[i]] = i
+
+obj = solution()
+result = obj.twosums([2,7,11,15] , 9)
+print(result)
