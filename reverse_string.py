e9a40bf55990e4cac69d0eda4680b28137c32f3f
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-10-18 09:57:34 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+class solution:
+    def rev_str(self,word):
+        left = 0
+        right = len(word) - 1
+
+        while left < right:
+            word[left],word[right] = word[right],word[left]
+            left = left+1
+            right = right -1
