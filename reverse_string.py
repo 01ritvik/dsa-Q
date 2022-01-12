12ecf60cfe8210e2a2edf504678e0d8267d03fa5
01ritvik <60423384+01ritvik@users.noreply.github.com>
2022-01-12 20:42:42 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -3,6 +3,7 @@ class solution:
         left = 0
         right = len(word) - 1
 
+        
         while left < right:
             word[left],word[right] = word[right],word[left]
             left = left+1
